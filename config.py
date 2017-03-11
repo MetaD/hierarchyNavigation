429857fe1cb3c062be06dc7dc2914526228b955b
Meng Du <mengdu@umich.edu>
2017-03-10 18:13:17 -0800

    generating trials

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
-# Numbers
-NUM_TRIALS = 3
+# Numbers/Experimental parameters
+NUM_TRIALS_PER_CONDITION = 4  # one condition == one face + one direction + one distance
+NUM_TRIALS_PER_RUN = 20
+NUM_FACES = 9
+DIRECTIONS = ('D', 'U')
+ANCHOR_INDEXES = [2, 3, 4, 5, 6]
+MIN_DISTANCE = 2
+DIR_COLORS = ('', '')
 # Paths
 IMG_FOLDER = 'img/'
 DATA_FOLDER = 'data/'
@@ -9,8 +15,7 @@ FIXATION_TIME_MIN = 1.5
 FIXATION_TIME_MAX = 3.5
 NUMBER_TIME = 0.5
 MENTAL_NAV_TIME = [3.5, 5.5]
-SELECTION_TIME = 0.4  # TODO ?
-POST_SELECTION_TIME = 0.1  # TODO ???
+CHOICE_TIME = 0.5
 FEEDBACK_TIME = 1.5
 TRIAL_INTERVAL = [4, 6, 8]
 # Strings
--- hierNavigation.py
+++ hierNavigation.py
@@ -4,11 +4,55 @@ from utilities import *
 from config import *
 
 
-def show_one_trial(images):
-    random_img_index = random.randrange(len(images) - 1)
-    response = presenter.select_from_two_stimuli(images[random_img_index], random_img_index,
-                                                 images[random_img_index + 1], random_img_index + 1)
-    return response
+def show_one_trial(param):
+    return {}
+
+
+def show_one_run():
+    pass
+
+
+def generate_trials():
+    # generate unique combinations
+    trial_list = []
+    for anchor in ANCHOR_INDEXES:
+        for dist in range(MIN_DISTANCE, NUM_FACES - MIN_DISTANCE):
+            if anchor - dist >= 0:
+                trial_list.append({
+                    'anchor': anchor,
+                    'direction': DIRECTIONS[0],
+                    'distance': dist
+                })
+            if anchor + dist < NUM_FACES:
+                trial_list.append({
+                    'anchor': anchor,
+                    'direction': DIRECTIONS[1],
+                    'distance': dist
+                })
+    trial_list *= NUM_TRIALS_PER_CONDITION
+    random.shuffle(trial_list)
+    # numbers
+    num_runs = len(trial_list) / NUM_TRIALS_PER_RUN
+    num_trials_per_anc_per_run = NUM_TRIALS_PER_RUN / len(ANCHOR_INDEXES)
+    num_trials_per_dir_per_run = NUM_TRIALS_PER_RUN / len(DIRECTIONS)
+    anchor_counter = [{anchor: 0 for anchor in ANCHOR_INDEXES} for i in range(num_runs)]
+    direc_counter = [{direc: 0 for direc in DIRECTIONS} for i in range(num_runs)]
+    trials = [[] for i in range(num_runs)]  # a list of runs
+    # append trials to runs
+    j = len(trial_list) - 1  # iterate from back to front
+    while j >= 0:
+        trial = trial_list[j]
+        for i in range(num_runs):
+            if anchor_counter[i][trial['anchor']] < num_trials_per_anc_per_run and \
+                            direc_counter[i][trial['direction']] < num_trials_per_dir_per_run and \
+                            trial not in trial[i]:
+                trials[i].append(trial_list.pop(j))
+                anchor_counter[i][trial['anchor']] += 1
+                direc_counter[i][trial['direction']] += 1
+                break
+        j -= 1
+    print trials
+    return trials
 
 
 def validation(items):
@@ -31,6 +75,7 @@ if __name__ == '__main__':
     sinfo = {'ID': '', 'Gender': ['Female', 'Male'], 'Age': '', 'Mode': ['Test', 'Exp']}
     show_form_dialog(sinfo, validation, order=['ID', 'Gender', 'Age', 'Mode'])
     sid = int(sinfo['ID'])
+    img_prefix = sinfo['Gender'][0]
 
     # create data file
     dataLogger = DataHandler(DATA_FOLDER, str(sid) + '.dat')
@@ -42,10 +87,18 @@ if __name__ == '__main__':
     presenter = Presenter(fullscreen=(sinfo['Mode'] == 'Exp'))
     dataLogger.write_data(presenter.expInfo)
     # load images
-    images = presenter.load_all_images(IMG_FOLDER, '.png')
-    random.shuffle(images)
+    images = presenter.load_all_images(IMG_FOLDER, '.jpg', img_prefix)
+    highlight = visual.ImageStim(presenter.window, image=IMG_FOLDER + 'highlight.png')
+    # randomize images
+    random.seed(sid)
+    random.shuffle(images)  # status high -> low
+    dataLogger.write_data({i: stim._imName for i, stim in enumerate(images)})
+
+    trials = generate_trials()
 
     # show trials
-    for t in range(NUM_TRIALS):
-        data = show_one_trial(images)
-        dataLogger.write_data({'trial_index': t, 'response': data})
+    for run in trials:
+        for trial in run:
+            data = show_one_trial(trial)
+            trial['response'] = data
+            dataLogger.write_data(trial)
